94c54f231356ec00bc5b7694ca8aea7adb15e69e
jsarasua <josarasua@gmail.com>
2021-02-05 14:29:09 -0600

    Updated tests and added references to to the project

--- src/DFS2Tests/DFS2Tests.cpp
+++ src/DFS2Tests/DFS2Tests.cpp
@@ -13,7 +13,11 @@ namespace DFS2Tests
 		
 		TEST_METHOD(TestMethod1)
 		{
-			Assert::AreEqual(true, true);
+			float distance0To1 = 1.f;
+			float distance0To1Test = GetDistance2D( Vec2( 0.f, 0.f ), Vec2( 1.f, 0.f ) );
+			bool isAlmostEqual0To1Test = AlmostEqualsFloat( distance0To1Test, distance0To1 );
+			Assert::AreEqual( isAlmostEqual0To1Test, true );
+
 		}
 	};
 }
